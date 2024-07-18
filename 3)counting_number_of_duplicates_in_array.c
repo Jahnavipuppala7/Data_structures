3eeff4e47b267d460d458f64da467597c15a5d2e
Jahnavipuppala7 <146073816+Jahnavipuppala7@users.noreply.github.com>
2024-07-18 18:10:30 +0530

    Update 3)counting_number_of_duplicates_in_array.c

--- 3)counting_number_of_duplicates_in_array.c
+++ 3)counting_number_of_duplicates_in_array.c
@@ -1,22 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,count=0;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++)
-    scanf("%d",&arr[i]);
-    for(int i=0;i<n;i++)
+int n,k,count=0;
+scanf("%d",&n); 
+int arr[n]; 
+for(int i=0;i<n;i++) 
+scanf("%d",&arr[i]); 
+for(int i=0;i<n;i++) 
+{
+    for(int j=i+1;j<n;j++) 
     {
-        for(int j=i+1;j<n-i;j++)
-        {
-            if(arr[i]==arr[j])
-            {
-                count=count+1;
-                for(k=j;k<n;k++)
-                arr[k]=arr[k+1];
-            }
-        }
+    if(arr[i]==arr[j]) 
+    { 
+        count=count+1; 
+    for(k=j;k<n-1;k++) 
+    {
+arr[k]=arr[k+1];
+        
     }
-    printf("%d",count);
+j--;
+n--;
+} 
+}}
+printf("%d",count);
 }
