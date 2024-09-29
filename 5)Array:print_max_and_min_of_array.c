419f8dfdb8c44050fcfe9067a869612cc61a65f9
Jahnavipuppala7 <146073816+Jahnavipuppala7@users.noreply.github.com>
2024-09-29 18:24:19 +0530

    Update 5)Array:print_max_and_min_of_array.c

--- 5)Array:print_max_and_min_of_array.c
+++ 5)Array:print_max_and_min_of_array.c
@@ -8,19 +8,14 @@ int main()
    {
        scanf("%d",&arr[i]);
    }
-   for(int i=0;i<n;i++)
+   min=arr[0];
+   max=arr[0];
+   for(int i=1;i<n;i++)
    {
-       for(int j=i+1;j<n;j++)
-       {
-           if(arr[i]<arr[j])
-           {
-               max=arr[j];
-               min=arr[i];
-           }
-           else
-           max=arr[i];
-           min=arr[j];
-       }
+           if(arr[i]>max)
+            max=arr[i];
+           if(arr[i]<min)
+           min=arr[i];
    }
    printf("max:%d\n",max);
    printf("min:%d",min);
